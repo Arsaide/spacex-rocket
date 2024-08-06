37ba0144fd88183ca16e483d2cd25034d88d7702
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-06 18:43:18 +0300

    worked with API
    - added Zustand store and settings it
    - added new space ships service and types

---
+++ src/api/models/SpaceShipsModels.ts
@@ -0,0 +1,75 @@
+export interface SpaceShipModel {
+    heat_shiel: {
+        material: string,
+        size_meters: number,
+        temp_degrees: number,
+        dev_partner: string
+    },
+    launch_payload_mass: {
+        kg: number,
+        lb: number
+    },
+    launch_payload_vol: {
+        cubic_meters: number,
+        cubic_feet: number
+    },
+    return_payload_mass: {
+        kg: number,
+        lb: number
+    },
+    return_payload_vol: {
+        cubic_meters: number,
+        cubic_feet: number
+    },
+    pressurized_capsule: {
+        payload_volume: {
+            cubic_meters: number,
+            cubic_feet: number
+        }
+    },
+    trunk: {
+        trunk_volume: {
+            cubic_meters: number,
+            cubic_feet: number
+        },
+        cargo: {
+            solar_array: number,
+            unpressurized_cargo: boolean
+        }
+    },
+    height_w_trunk: {
+        meters: number,
+        feet: number
+    },
+    diameter: {
+        meters: number,
+        feet: number
+    },
+    first_flight: string,
+    flickr_images: string[],
+    name: string,
+    type: string,
+    active: boolean,
+    crew_capacity: number,
+    sidewall_angle_deg: number,
+    orbit_duration_yr: number,
+    dry_mass_kg: number,
+    dry_mass_lb: number,
+    thrusters: ThrustersTypes[],
+    wikipedia: number,
+    description: string,
+    id: string
+}
+
+interface ThrustersTypes {
+    type: string,
+    amount: number,
+    pods: number,
+    fuel_1: string,
+    fuel_2: string,
+    isp: number,
+    thrust: {
+        kN: number,
+        lbf: number
+    }
+}
---
+++ src/api/services/SpaceShipsService.ts
@@ -0,0 +1,13 @@
+import { AxiosResponse } from 'axios';
+import $api from '../request';
+import { SpaceShipModel } from '../models/SpaceShipsModels.ts';
+
+export default class SpaceShipsService {
+    static async getAllSpaceShips(): Promise<AxiosResponse<SpaceShipModel[]>> {
+        return $api.get<SpaceShipModel[]>(`/v4/dragons`);
+    }
+
+    static async getOneSpaceShip(id: string): Promise<AxiosResponse<SpaceShipModel>> {
+        return $api.get<SpaceShipModel>(`/v4/dragons/${id}`);
+    }
+}
