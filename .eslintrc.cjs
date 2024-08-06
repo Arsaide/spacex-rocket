1015f557486abaa46c4fc81aa5088b72faee80e4
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-06 20:54:17 +0300

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,7 +37,7 @@ module.exports = {
     ],
     'unused-imports/no-unused-imports': 'error',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
     "import/named": "off"
   },
 }
