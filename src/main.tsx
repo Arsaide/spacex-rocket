9d2310ed26a02aedc77a1a7474857adc17b01337
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-06 21:48:22 +0300

    added new packages and fixed bugs
    - added tanstack queries package
    - fixed router dom bugs

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.scss';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './utils/routes/routes.tsx';
+import router from './utils/routes/routes.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     </React.StrictMode>,
 );
