da4600c653712016b5b906734af8f91ca498c301
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-06 18:35:06 +0300

    settings eslint and prettier
    - updated eslint rules
    - added and settings prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,31 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier"
   ],
+  settings: {
+    react: {
+      version: "detect"
+    },
+    "import/resolver": {
+      "node": {
+        "paths": [
+          "src"
+        ],
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'unused-imports'],
@@ -16,5 +37,6 @@ module.exports = {
     ],
     'unused-imports/no-unused-imports': 'error',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
+    "import/named": "off"
   },
 }
