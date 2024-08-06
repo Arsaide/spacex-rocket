7a80a80744d5cc2da6e0df709e1453adc48cdc59
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-06 18:05:35 +0300

    added new packages
    - added axios and types
    - added react-router-dom
    - added sass
    - added zustand
    - added many eslint plugins
    - added prettier
    - added test routes path
    - refactoring code

---
+++ src/api/request/index.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+// /v4/dragons
+const API_URL = `https://api.spacexdata.com`;
+// const API_URL = 'http://localhost:8000'
+
+const $api = axios.create({
+    withCredentials: true,
+    baseURL: API_URL,
+});
+
+$api.interceptors.request.use(config => {
+    // config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+});
+
+export default $api;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.scss';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './utils/routes/routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+);
