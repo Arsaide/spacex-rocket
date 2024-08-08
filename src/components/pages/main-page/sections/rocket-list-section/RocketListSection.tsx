aabf2d40a166f86f1df33298d1354f0b0e1c679d
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-09 00:16:41 +0300

    fixed containers and styles
    - fixed containers styles

--- src/components/pages/main-page/sections/rocket-list-section/RocketListSection.tsx
+++ src/components/pages/main-page/sections/rocket-list-section/RocketListSection.tsx
@@ -1,25 +1,27 @@
 import classNames from 'classnames';
-import styles from './RocketListSection.module.scss'
+import styles from './RocketListSection.module.scss';
 import RocketSwiper from './components/rocket-swiper/RocketSwiper.tsx';
 import { useGetAllRockets } from '../../../../../hooks/queries/use-get-all-rockets/useGetAllRockets.ts';
 import RocketListSkeleton from './components/rockets-list-skeleton/RocketListSkeleton.tsx';
 
 const RocketListSection = () => {
-    const {data, isLoading, isError, error} = useGetAllRockets()
+    const { data, isLoading, isError, error } = useGetAllRockets();
 
     return (
         <section className={styles.section}>
-            <div className={classNames(styles.content)}>
-                <h2 className={styles.title}>Our rockets</h2>
-                {isLoading ? (
-                    <RocketListSkeleton/>
-                ) : isError ? (
-                    <>Something went wrong... Error: {error?.message}</>
-                ) : data ? (
-                    <div>
+            <div className={'cnt'}>
+                <div className={classNames(styles.content)}>
+                    <h2 className={styles.title}>Our rockets</h2>
+                    {isLoading ? (
+                        <RocketListSkeleton />
+                    ) : isError ? (
+                        <>Something went wrong... Error: {error?.message}</>
+                    ) : data ? (
+                        <div>
                             <RocketSwiper rockets={data} />
-                    </div>
-                ): null}
+                        </div>
+                    ) : null}
+                </div>
             </div>
         </section>
     );
