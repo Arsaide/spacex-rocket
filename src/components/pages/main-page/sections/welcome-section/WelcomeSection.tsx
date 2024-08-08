1afbf74bac508a3c93da26f1fb937483ea2aea44
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-09 00:16:41 +0300

    fixed containers and styles
    - fixed containers styles

--- src/components/pages/main-page/sections/welcome-section/WelcomeSection.tsx
+++ src/components/pages/main-page/sections/welcome-section/WelcomeSection.tsx
@@ -1,16 +1,17 @@
 import styles from './WelcomeSection.module.scss';
 import WelcomeHeader from './components/welcome-header/WelcomeHeader.tsx';
-import classNames from 'classnames';
 import WelcomeContent from './components/welcome-content/WelcomeContent.tsx';
 import WelcomeBottom from './components/welcome-bottom/WelcomeBottom.tsx';
 
 const WelcomeSection = () => {
     return (
         <section className={styles.section}>
-            <div className={classNames('cnt', styles.content)}>
-                <WelcomeHeader />
-                <WelcomeContent />
-                <WelcomeBottom />
+            <div className={'cnt'}>
+                <div className={styles.content}>
+                    <WelcomeHeader />
+                    <WelcomeContent />
+                    <WelcomeBottom />
+                </div>
             </div>
         </section>
     );
