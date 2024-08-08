e8a983349fcd4e2ec9d36d2b9c07d1261044691f
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-08 23:10:38 +0300

    added counter section, updated configs and package jsons
    - added counter section, worked with counters and observers
    - working with styles
    - updated configs and packages
    - updated api

--- src/components/pages/main-page/Main.tsx
+++ src/components/pages/main-page/Main.tsx
@@ -1,11 +1,13 @@
 import WelcomeSection from './sections/welcome-section/WelcomeSection.tsx';
 import RocketListSection from './sections/rocket-list-section/RocketListSection.tsx';
+import CounterSection from './sections/counters-section/CounterSection.tsx';
 
 const MainPage = () => {
     return (
         <>
             <WelcomeSection />
             <RocketListSection />
+            <CounterSection/>
         </>
     );
 };
---
+++ src/components/pages/main-page/sections/counters-section/counter/Counter.tsx
@@ -0,0 +1,68 @@
+import { FC, useEffect, useRef, useState } from 'react';
+import styles from './Counter.module.scss'
+
+interface CounterProps {
+    value: number;
+    label: string;
+    interval: number;
+}
+
+const Counter:FC<CounterProps> = ({value, label, interval}) => {
+    const [count, setCount] = useState(0);
+    const [isVisible, setIsVisible] = useState(false);
+    const counterRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                    observer.disconnect();
+                }
+            },
+            { threshold: 0.1 }
+        );
+
+        if (counterRef.current) {
+            observer.observe(counterRef.current);
+        }
+
+        return () => observer.disconnect();
+    }, []);
+
+    useEffect(() => {
+        if (isVisible) {
+            let startNumber = 0;
+            const countInterval = interval;
+            const i = 1;
+
+            const counterUp = () => {
+                startNumber += i;
+                if (startNumber >= value) {
+                    setCount(value);
+                    return true;
+                } else {
+                    setCount(startNumber);
+                    return false;
+                }
+            };
+
+            const timer = setInterval(() => {
+                if (counterUp()) {
+                    clearInterval(timer);
+                }
+            }, countInterval);
+
+            return () => clearInterval(timer);
+        }
+    }, [isVisible, value]);
+
+    return (
+        <div ref={counterRef} className={styles.counter}>
+            <div className={styles.value}>{count}</div>
+            <div className={styles.label}>{label}</div>
+        </div>
+    );
+};
+
+export default Counter;
