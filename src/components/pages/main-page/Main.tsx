5d931b716d01ae46c5f3ac6174a7b0d718672d8f
Kirill Murahovskiy <93765186+Arsaide@users.noreply.github.com>
2024-08-08 23:59:05 +0300

    added about us video section
    - added about us video section
    - working with styles

--- src/components/pages/main-page/Main.tsx
+++ src/components/pages/main-page/Main.tsx
@@ -1,13 +1,15 @@
 import WelcomeSection from './sections/welcome-section/WelcomeSection.tsx';
 import RocketListSection from './sections/rocket-list-section/RocketListSection.tsx';
 import CounterSection from './sections/counters-section/CounterSection.tsx';
+import AboutUsSection from './sections/about-us-section/AboutUsSection.tsx';
 
 const MainPage = () => {
     return (
         <>
             <WelcomeSection />
             <RocketListSection />
-            <CounterSection/>
+            <CounterSection />
+            <AboutUsSection />
         </>
     );
 };
